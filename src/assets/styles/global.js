3a93f7d5ee3b40e1bb9f18296153ed0f3678a9ca
ddhairya <d_dhairya@yahoo.com>
2021-05-03 18:36:30 +0400

    Navbar v0.0.1

---
+++ src/assets/styles/global.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const Style = styled.div`
+    .brandlogo{
+        width:64px;
+    }
+    .brandtitle{
+        height:64px;
+        font-size: 36px;
+        vertical-align: top;
+    }
+    .navbar {
+        background-color: #ffce02;
+        padding: 1rem;
+    }
+    .navbar-brand{
+        text-decoration: none;
+        color:white;
+    }
+`;
+
+export default Style;
---
+++ src/components/Navigation.js
@@ -0,0 +1,40 @@
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import brandlogo from "../assets/images/logo.png";
+import styled from "styled-components";
+import GlobalStyle from "../assets/styles/global";
+
+
+const Style = styled.div`
+.brandlogo{
+    
+}
+
+`;
+
+
+
+const Navigation = () => {
+    return(
+
+        <GlobalStyle>
+            <Style>
+                <Navbar bg="light" expand="lg">
+                    <div>
+                        <Navbar.Brand href="/">
+                            <img className='brandlogo' src={brandlogo} alt="knowledge" />
+                            <span className='brandtitle'> knowλεδγε | egdelwΩνκ</span>
+                        </Navbar.Brand>
+                        
+                    </div>
+                    
+                </Navbar>
+                <p class='title'>Hello</p>
+            </Style>
+        </GlobalStyle>
+
+    )    
+}
+
+
+export default Navigation;
